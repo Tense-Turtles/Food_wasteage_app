dbf57baea08ac263721dd7a100fe17fc5a0207e5
Beast-Sanjay <sanjuniky2002@gmail.com>
2023-01-31 17:06:35 +0530

    home page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_management/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-     home: //Add the home screen page
+     home: HomePage(),//Add the home screen page
     );
   }
 }
