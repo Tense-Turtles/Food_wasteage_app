c806a962cf7039ed53ac8a1059906b551dea312f
Beast-Sanjay <sanjuniky2002@gmail.com>
2023-02-14 19:36:18 +0530

    horizontal scrollable added for information

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,67 +11,75 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(child: Text('Hello')),
-        // backgroundColor: const Color(0xFFFDFFE8),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text("Home",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            children: [
-              const Text(
-                'Welcome to Food care',
-                style: TextStyle(fontSize: 0),
-                textAlign: TextAlign.left,
-              ),
-              // color: Colors.white,
-              // alignment: Alignment.topLeft,
-              // padding: const EdgeInsets.all(10),
-              // height: 80,
+      drawer: Drawer(child: Text('Hello')),
+      // backgroundColor: const Color(0xFFFDFFE8),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Home",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Hi Shailesh",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ]),
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: [
+                    for (int i = 0; i < 6; i++)
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 180,
+                        width: 350,
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const ButtonBar(),
+                      ),
+                  ])),
+            ]),
+      ),
 
-              Container(
-                height: 150,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10)),
-              )
-            ],
-          ),
-        )
-        // CustomScrollView(
-        //   slivers: [
-        //     SliverAppBar.large(
-        //       backgroundColor: const Color.fromARGB(255, 0, 79, 45),
-        //       // leading: const Icon(Icons.menu),
-        //       title: const Text('F o o d  C a r e',
-        //           style: TextStyle(
-        //               fontStyle: FontStyle.italic,
-        //               fontWeight: FontWeight.bold,
-        //               color: Colors.white)),
-        //     ),
-        //     for (int i = 0; i < 6; i++)
-        //       SliverToBoxAdapter(
-        //         child: Padding(
-        //           padding: const EdgeInsets.only(
-        //             top: 20.0,
-        //             left: 20,
-        //             right: 20,
-        //           ),
-        //           child: ClipRRect(
-        //             borderRadius: BorderRadius.circular(10),
-        //             child: Container(
-        //               color: const Color.fromARGB(255, 204, 255, 146),
-        //               height: 150,
-        //             ),
-        //           ),
-        //         ),
-        //       ),
-        //   ],
-        // ),
-        );
+      // CustomScrollView(
+      //   slivers: [
+      //     SliverAppBar.large(
+      //       backgroundColor: const Color.fromARGB(255, 0, 79, 45),
+      //       // leading: const Icon(Icons.menu),
+      //       title: const Text('F o o d  C a r e',
+      //           style: TextStyle(
+      //               fontStyle: FontStyle.italic,
+      //               fontWeight: FontWeight.bold,
+      //               color: Colors.white)),
+      //     ),
+      //     for (int i = 0; i < 6; i++)
+      //       SliverToBoxAdapter(
+      //         child: Padding(
+      //           padding: const EdgeInsets.only(
+      //             top: 20.0,
+      //             left: 20,
+      //             right: 20,
+      //           ),
+      //           child: ClipRRect(
+      //             borderRadius: BorderRadius.circular(10),
+      //             child: Container(
+      //               color: const Color.fromARGB(255, 204, 255, 146),
+      //               height: 150,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //   ],
+      // ),
+    );
     // return Scaffold(
     //   // appBar: AppBar(
     //   //   title: Text(
