2c74b385abf4f23b741d59826754ee0e397c25fc
Beast-Sanjay <sanjuniky2002@gmail.com>
2023-02-15 11:44:58 +0530

    removed unnessary comments

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -125,23 +125,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                 }).toList(),
               ),
 
-              // SingleChildScrollView(
-              //     scrollDirection: Axis.horizontal,
-              //     child: Row(children: [
-              //       Container(
-              //           child: CarouselSlider(
-              //         options: CarouselOptions(),
-              //         items: imgList
-              //             .map((item) => Container(
-              //                   child: Center(
-              //                       child: Image.network(item,
-              //                           fit: BoxFit.cover, width: 100)),
-              //                 ))
-              //             .toList(),
-              //       )),
-              //     ])
-
-              //     ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Container(
@@ -213,64 +196,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                   ],
                 ),
               )
-              //const TabBarView(children: [Center(child: Text('hello'))])
+             
             ]),
       ),
     );
   }
 }
-
-// class CarouselWithIndicatorDemo extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _CarouselWithIndicatorState();
-//   }
-// }
-
-// class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
-//   int _current = 0;
-//   final CarouselController _controller = CarouselController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//      // appBar: AppBar(title: Text('Carousel with indicator controller demo')),
-//       body: Column(children: [
-//         Expanded(
-//           child: CarouselSlider(
-//             items: imageSliders,
-//             carouselController: _controller,
-//             options: CarouselOptions(
-//                 autoPlay: true,
-//                 enlargeCenterPage: true,
-//                 aspectRatio: 2.0,
-//                 onPageChanged: (index, reason) {
-//                   setState(() {
-//                     _current = index;
-//                   });
-//                 }),
-//           ),
-//         ),
-//         Row(
-//          // mainAxisAlignment: MainAxisAlignment.center,
-//           children: imgList.asMap().entries.map((entry) {
-//             return GestureDetector(
-//               onTap: () => _controller.animateToPage(entry.key),
-//               child: Container(
-//                 width: 12.0,
-//                 height: 12.0,
-//                 margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-//                 decoration: BoxDecoration(
-//                     shape: BoxShape.circle,
-//                     color: (Theme.of(context).brightness == Brightness.dark
-//                             ? Colors.white
-//                             : Colors.black)
-//                         .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-//               ),
-//             );
-//           }).toList(),
-//         ),
-//       ]),
-//     );
-//   }
-// }
